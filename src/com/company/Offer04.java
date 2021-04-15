c97cf1b9a4772c8b042aa2ccde7620ac29c0a7f5
xniu <xingxing.niu@leadstor.com>
2021-04-15 15:49:44 +0800

    feat(04): add problem 04

---
+++ src/com/company/Offer04.java
@@ -0,0 +1,48 @@
+package com.company;
+
+/**
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 现有矩阵 matrix 如下：
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ *
+ * 给定 target = 20，返回 false。
+ *
+ */
+public class Offer04 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,4,7,11,15}, {2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target1 = 5;
+        int target2 = 20;
+
+        System.out.println(findNumberIn2DArray(arr, target1));
+        System.out.println(findNumberIn2DArray(arr, target2));
+    }
+
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
